e0a4448252582241ce34a942d2edf3974de58e22
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-11-28 13:06:38 +0100

    ADD/AppVew - hanlding user inputs and outputs

--- src/App/view/AppView.java
+++ src/App/view/AppView.java
@@ -1,4 +1,24 @@
 package App.view;
 
+import java.util.Scanner;
+
 public class AppView {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String getFileContent() {
+        System.out.print("Enter file content: ");
+
+        return scanner.nextLine().trim();
+    }
+    public String getFileName() {
+        System.out.print("Enter file name: ");
+
+        return scanner.nextLine().trim();
+    }
+    public void closeScanner() {
+        scanner.close();
+    }
+    public void writeOutput(String item) {
+        System.out.print(item + " ");
+    }
 }
